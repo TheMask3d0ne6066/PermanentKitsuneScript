3155fffb25219bc9c781d7ef3ae395153c0656cc
TheMask3d0ne6066 <thatmaskedone6066@gmail.com>
2024-10-03 13:21:35 +0530

    Create KitsuneFruitScript.lua

---
+++ KitsuneFruitScript.lua
@@ -0,0 +1,6 @@
+local Player = game.Players.LocalPlayer
+local Backpack = Player:WaitForChild("Backpack")
+
+local kitsuneFruit = Instance.new("KitsuneFruit")
+kitsuneFruit.Name = "Permanent Kitsune"
+kitsuneFruit.Parent = Backpack
